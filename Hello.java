3f7b1531b10a3f13f12e1c4158f7ea367c73b042
yoojxygit1 <1311591744@qq.com>
2018-03-20 17:04:34 +0800

    master modify

--- Hello.java
+++ Hello.java
@@ -15,7 +15,13 @@ public class HelloWorld {
 	 public static void main(String[] args) {
         System.out.println("Hello World");
 		System.out.println("Helloxxxxxx");
-		
+		public static void main(String[] args) {
+        System.out.println("Hello World");
+		System.out.println("Helloxxxxxx");public static void main(String[] args) {
+        System.out.println("Hello World");
+		System.out.println("Helloxxxxxx");public static void main(String[] args) {
+        System.out.println("Hello World");
+		System.out.println("Helloxxxxxx");
 		
     }
 	 public static void main(String[] args) {
