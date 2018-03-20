38820a3476766f3de647a856dcef4029816b48ed
yoojxygit1 <1311591744@qq.com>
2018-03-20 17:00:33 +0800

    master...

--- Hello.java
+++ Hello.java
@@ -5,10 +5,7 @@ public class HelloWorld {
         System.out.println("Hello World");
 		System.out.println("Helloxxxxxx");
 		System.out.println("*****************************");
-		System.out.println("*********");
-		System.out.println("**00000000000000");
-		System.out.println("*****************************");
-		System.out.println("5555555555555555555555555555");
+		
 	public static void print()
 	{
 		 System.out.println("Hello modified master");
