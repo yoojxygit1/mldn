cf94822ae642525cf7fedf01cc3f32c6545ee032
yoojxygit1 <1311591744@qq.com>
2018-03-20 16:49:02 +0800

    master modify

--- Hello.java
+++ Hello.java
@@ -7,5 +7,17 @@ public class HelloWorld {
 		
 		
     }
+	 public static void main(String[] args) {
+        System.out.println("Hello World");
+		System.out.println("Helloxxxxxx");
+		
+		
+    }
+	 public static void main(String[] args) {
+        System.out.println("Hello World");
+		System.out.println("Helloxxxxxx");
+		
+		
+    }
 }
 
