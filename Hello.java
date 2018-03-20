21ff1bda34e94cbc412127475b4b8e77b270f380
yoojxygit1 <1311591744@qq.com>
2018-03-20 17:02:49 +0800

    brh...

--- Hello.java
+++ Hello.java
@@ -12,17 +12,6 @@ public class HelloWorld {
 	}
 		
     }
-	 public static void main(String[] args) {
-        System.out.println("Hello World");
-		System.out.println("Helloxxxxxx");
-		
-		
-    }
-	 public static void main(String[] args) {
-        System.out.println("Hello World");
-		System.out.println("Helloxxxxxx");
-		
-		
-    }
+	
 }
 
